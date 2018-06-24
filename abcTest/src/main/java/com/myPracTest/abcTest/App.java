20fb1db904a77b3d8b5f8fc59d5f2b408c680d99
Kumar <kktestingnj@gmail.com>
2018-06-24 16:07:12 -0700

    my last commit from new eclipse

--- abcTest/src/main/java/com/myPracTest/abcTest/App.java
+++ abcTest/src/main/java/com/myPracTest/abcTest/App.java
@@ -11,9 +11,14 @@ public class App
         System.out.println( "Hello World!" );
         
         System.out.println("This is second checking");
+
         
         //Testing the eclipse and github integration
         
         System.out.println("This is myold eclispe commit");
+
+        //Abc testing ne eclipse changes
+        System.out.println("This is new  eclispe commit");
+
     }
 }
